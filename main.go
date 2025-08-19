a1389f0d870be9c58a3e6313ceeab919e59a5ac4
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-08-20 00:02:48 +0330

    feat: add username input validation and error handling
    Add getUserName function to validate command line arguments and return appropriate errors. The main function now handles these errors and prints the username when provided correctly.

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	userName, err := getUserName()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("your username is", userName)
+}
+
+func getUserName() (string, error) {
+	switch len(os.Args) {
+	case 2:
+		return os.Args[1], nil
+	case 1:
+		return "", errors.New("username is required")
+	default:
+		return "", errors.New("invalid number of flags")
+	}
 }
