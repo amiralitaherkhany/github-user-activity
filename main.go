e42f21a319bf83b4685227e62a23f2ba2d90c90a
amiralitaherkhany <amiralitaherkhany07@gmail.com>
2025-08-23 22:33:18 +0330

    feat: add GitHub activity tracking for users
    Add functionality to fetch and display a user's GitHub activity count by calling the GitHub API. This extends the existing username feature to provide more user insights.

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -13,6 +16,32 @@ func main() {
 		log.Fatalln(err)
 	}
 	fmt.Println("your username is", userName)
+	numberOfActivities, err := getUserGithubActivity(userName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Number of activities is", numberOfActivities)
+}
+
+func getUserGithubActivity(username string) (uint64, error) {
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", username)
+	resp, err := http.Get(url)
+	if err != nil {
+		return 0, err
+	}
+	if resp.StatusCode != 200 {
+		return 0, fmt.Errorf("Response failed with status code: %d\n", resp.StatusCode)
+	}
+	body, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+
+	var data []interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return 0, errors.New("error in parsing data")
+	}
+	return uint64(len(data)), nil
+
 }
 
 func getUserName() (string, error) {
